e58a8c3394e332d62a2f0509817c34a47c3b7598
fadilmuh22 <fadilmuh22@gmail.com>
2023-06-12 13:46:33 +0700

    feat: menghotel finishing

--- src/Types/TypeHotelDetails.ts
+++ src/Types/TypeHotelDetails.ts
@@ -1,28 +1,27 @@
 declare global {
-    type TypeRoom = {
-        id: number;
-        title: string;
-        description: string;
-        photo: string;
-        price: number;
-    };
-    type TypeScenic = {
-        id: number;
-        title: string;
-        photo: string;
-        price_rate: number;
-    };
+  type TypeRoom = {
+    id: number;
+    title: string;
+    description: string;
+    photo: string;
+    price: number;
+  };
+  type TypeScenic = {
+    id: number;
+    title: string;
+    photo: string;
+    price_rate: number;
+  };
 
-    type TypeHotelDetails = {
-        id: string;
-        hotel_id: number;
-        city: string;
-        possibilities: string[];
-        max_adult_size: number;
-        child_status: boolean;
-        room_type: TypeRoom[];
-        room_scenic: TypeScenic[];
-    };
+  type TypeHotelDetails = {
+    id: string;
+    hotel_id: number;
+    city: string;
+    max_adult_size: number;
+    child_status: boolean;
+    room_type: TypeRoom[];
+    room_scenic: TypeScenic[];
+  };
 }
 
 export {};
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import App from './containers/App';
-import reportWebVitals from './reportWebVitals';
-import store from './stores';
+import App from "./containers/App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./stores";
 
-import './lib/styles/_reboot.css';
-import './lib/styles/main.scss';
+import "./lib/styles/_reboot.css";
+import "./lib/styles/main.scss";
+
+import "@fontsource/poppins/300.css";
+import "@fontsource/poppins/400.css";
+import "@fontsource/poppins/500.css";
+import "@fontsource/poppins/700.css";
 
 ReactDOM.render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <App />
-        </Provider>
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 reportWebVitals();
